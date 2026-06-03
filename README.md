fa43f7f475a7aec19955dcfde7ab3e113dfaeab2
yash-learner <yash.topaz@gmail.com>
2026-06-03 20:11:46 +0530

    Create the plug using the create-care-mfe-plug

---
+++ README.md
@@ -0,0 +1,1 @@
+care_demo_facility_setup_fe - Care FE Plugin
---
+++ src/hooks/useTranslation.ts
@@ -0,0 +1,5 @@
+import { useTranslation as useTranslationBase } from "react-i18next";
+
+export const useTranslation = () => {
+  return useTranslationBase("care_demo_facility_setup_fe_fe");
+};
